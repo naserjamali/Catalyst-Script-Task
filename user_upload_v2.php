3b73726c76f4e0a2ff18240899460e6a2a07fe39
naserjamali <naserjamali@hotmail.com>
2016-02-10 12:06:12 +1100

    Update user_upload_v2.php

--- user_upload_v2.php
+++ user_upload_v2.php
@@ -3,22 +3,27 @@ $servername = "localhost";
 $username = "root";
 $password = "***password***";
 $dbname = "company";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
+
 // sql to create table
 $sql = "CREATE TABLE users (
 name VARCHAR(30) NOT NULL,
 surname VARCHAR(30) NOT NULL,
 email VARCHAR(50) NOT NULL UNIQUE
 )";
+
 if ($conn->query($sql) === TRUE) {
 echo "Table users created successfully!";
 } else {
 echo "Error creating table: " . $conn->error;
 }
+
 $conn->close();
+
 ?>
