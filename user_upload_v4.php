0e59a9e4950106236e049a8584c198c94bb75b75
naserjamali <naserjamali@hotmail.com>
2016-02-10 14:28:30 +1100

    Update user_upload_v4.php

--- user_upload_v4.php
+++ user_upload_v4.php
@@ -3,20 +3,26 @@ echo "Uploading...<br>";
 
    $fp = fopen("users.csv","r") or die("can not open file");
 
-echo "CSV file is open...";
-
+echo "CSV file is open...<br>";
 
 while ($csv_line = fgetcsv($fp,1024)) {
 
-   $str1 = trim("$csv_line[0]");
-   $str2 = trim("$csv_line[1]");
-   $str3 = trim("$csv_line[2]");
+   $name = ucfirst(strtolower(trim("$csv_line[0]")));
+   $surname = ucfirst(strtolower(trim("$csv_line[1]")));
+   $email = strtolower(trim("$csv_line[2]"));
 
-if($str1.$str2.$str3 == "namesurnameemail"){
+if($name.$surname.$email == "NameSurnameemail"){
 continue;
 } 
 
-print $str1.">>>".$str2.">>>".$str3. "<BR>";
+print $name.">>>".$surname.">>>".$email;
+
+// Validating email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+     echo(">>>$email is a valid email address <br>");
+} else {
+     echo(">>>$email is not a valid email address <br>");
+}
 
 }
 
